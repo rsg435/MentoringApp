d8bbd12ea3a3256ee72787b7a3c93358a2510eaa
Rachel Parker <rsgaguin@gmail.com>
2023-11-27 20:08:35 +0000

    Initialise new project

---
+++ MentoringApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MentoringApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
