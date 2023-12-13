d91c90362316a1d6ccaf3a03b83438e1b7d68cee
Rachel Parker <rsgaguin@gmail.com>
2023-12-13 13:12:42 +0000

    Added notification repository to unit of work

--- MentoringApp/Data/Repository/UnitOfWork.cs
+++ MentoringApp/Data/Repository/UnitOfWork.cs
@@ -1,5 +1,6 @@
 ﻿using MentoringApp.Data;
 using MentoringApp.Data.Repository;
+using MentoringApp.Data.Repository.IRepository;
 using MentoringApp.Repository.IRepository;
 
 namespace MentoringApp.Repository
@@ -11,12 +12,14 @@ namespace MentoringApp.Repository
         public IStudentRepository Student { get; private set; }
         public IUniversityRepository University { get; private set; }
         public IConnectionRepository Connection { get; private set; }
+        public INotificationRepository Notification { get; private set; }
         public UnitOfWork(ApplicationDbContext context)
         {
             _context = context;
             Student = new StudentRepository(_context);
             University = new UniversityRepository(_context);
             Connection = new ConnectionRepository(_context);
+            Notification = new NotificationRepository(_context);
         }
 
         public void Save()
