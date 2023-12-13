85cbb26ebc0dd30cca8813045a7688bb901b63ff
Rachel Parker <rsgaguin@gmail.com>
2023-12-13 15:59:50 +0000

    Added notifications controller and view

---
+++ MentoringApp/Controllers/NotificationController.cs
@@ -0,0 +1,37 @@
+﻿using MentoringApp.Data.DTOs;
+using MentoringApp.Repository;
+using MentoringApp.Repository.IRepository;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+
+namespace MentoringApp.Controllers
+{
+	[Authorize]
+	public class NotificationController : Controller
+	{
+		private readonly IUnitOfWork _unitOfWork;
+		private IHttpContextAccessor _httpContextAccessor;
+		private readonly string _currentUserId;
+
+		public NotificationController(IUnitOfWork unitOfWork, IHttpContextAccessor httpContextAccessor)
+        {
+			_unitOfWork = unitOfWork;
+			_httpContextAccessor = httpContextAccessor;
+			_currentUserId = _httpContextAccessor.HttpContext.User.FindFirst(ClaimTypes.NameIdentifier).Value;
+		}
+        public IActionResult Index()
+		{
+			return View();
+		}
+
+		public JsonResult GetNotifications(bool getOnlyUnread)
+		{
+			var notifications = new List<NotificationDto>();
+			notifications = _unitOfWork.Notification.GetNotifications(_currentUserId, getOnlyUnread);
+			return Json(notifications);
+			
+		}
+	}
+}
--- MentoringApp/Data/Repository/IRepository/IUnitOfWork.cs
+++ MentoringApp/Data/Repository/IRepository/IUnitOfWork.cs
@@ -1,10 +1,13 @@
-﻿namespace MentoringApp.Repository.IRepository
+﻿using MentoringApp.Data.Repository.IRepository;
+
+namespace MentoringApp.Repository.IRepository
 {
     public interface IUnitOfWork
     { 
         IStudentRepository Student { get; }
         IUniversityRepository University { get; }
         IConnectionRepository Connection { get; }
+        INotificationRepository Notification { get; }
         void Save();
     }
 }
